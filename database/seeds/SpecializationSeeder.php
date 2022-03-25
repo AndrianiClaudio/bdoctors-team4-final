b0cc9e3a81d6264480774935884bc66055fe5ddb
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-03-25 20:55:23 +0100

    create empty migration, model, seeder

---
+++ database/seeds/SpecializationSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SpecializationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}

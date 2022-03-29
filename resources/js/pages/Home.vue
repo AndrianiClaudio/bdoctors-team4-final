b83878bff7a41b1f3492115002d758931c37dc7b
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-03-29 20:06:06 +0200

    home.vue get all doctors

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,12 +1,63 @@
 <template>
-  <div class="container">
+<div class="container">
     Home
-  </div>
+    <div v-if="doctors">
+        <div v-for="doctor in doctors" :key="doctor.id">
+            <h1>Dottore {{doctor.id}}</h1>
+            <div>
+                <b><em>Nome</em></b>
+                {{doctor.firstname}} {{doctor.lastname}} 
+            </div>
+            <div v-if="doctor.photo">
+                <b><em>Photo</em></b>
+                <!-- da includere in tag img -->
+                {{doctor.photo}}
+            </div>
+            <div>
+                <b><em>Email</em></b>
+                {{doctor.email}}
+            </div>
+            <div>
+                <b><em>Address</em></b>
+                {{doctor.address}}
+            </div>
+            <div v-if="doctor.cv">
+                <b><em>Curriculum Vitae</em></b>
+                {{doctor.address}}
+            </div>
+            <div v-if="doctor.phone">
+                <b><em>Telefono</em></b>
+                {{doctor.phone}}
+            </div>
+            <hr>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+    name: 'Home',
+    data () {
+        return {
+        doctors: {
+        type: Array,
+        },
+    }
+    },
+    methods: {
+    getAllDoctors() {
+        axios.get('api/doctors').
+        then((res) => {
+        this.doctors = res.data.doctors
+        }).catch((err) => {
+        console.error(err);
+        });
+    }
+    },
+    created() {
+    this.getAllDoctors();
+    }
 }
 </script>
 

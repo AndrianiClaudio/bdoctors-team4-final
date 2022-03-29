7124e1319a39bae20f6610d46b25d5d111127aad
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-03-29 22:01:21 +0200

    add calendar

--- resources/js/front.js
+++ resources/js/front.js
@@ -12,6 +12,7 @@ window.Vue = require('vue');
 // IMPORT COMPONENTS
 import App from './view/App.vue';
 import Home from './pages/Home.vue';
+import  EventCalendar from './components/EventCalendar.vue'
 // IMPORT ROUTER ... NEXT
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
@@ -23,6 +24,12 @@ const router = new VueRouter({
             path: '/',
             name: 'home',
             component: Home,
+            
+        },
+        {
+            path: '/calendar',
+            name: 'calendar',
+            component: EventCalendar,
             
         },
     ]
--- resources/js/view/App.vue
+++ resources/js/view/App.vue
@@ -2,11 +2,16 @@
   <div class="container">
     App
     <router-view></router-view>
+    <EventCalendar/>
   </div>
 </template>
 
 <script>
+// import  EventCalendar from '../components/EventCalendar.vue'
 export default {
+  // components: {
+  //   EventCalendar
+  //   },
   name: 'App',
 }
 </script>

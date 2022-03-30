8ab9d2e93d6be1c85250833cfbd899e041a35996
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-03-30 16:34:37 +0200

    messages validation Vue

--- resources/js/pages/Message.vue
+++ resources/js/pages/Message.vue
@@ -3,7 +3,17 @@
         <form
             :action="`http://localhost:8000/api/doctors/${doctor.slug}/new`"
             method="post"
+            @submit="checkForm($event)"
         >
+
+
+          <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="(error,index) in errors" :key="index">{{ error }}</li>
+    </ul>
+  </p>
+
             <!-- FIRSTNAME -->
             <div class="mb-3">
                 <label for="firstname" class="form-label">firstname</label>
@@ -13,6 +23,7 @@
                     id="firstname"
                     placeholder="firstname"
                     name="firstname"
+                    v-model="firstname"
                 />
                 <div class="invalid-feedback">Looks good!</div>
                 <!-- <div class="invalid-feedback">Please choose a username.</div> -->
@@ -26,6 +37,7 @@
                     id="lastname"
                     placeholder="lastname"
                     name="lastname"
+                    v-model="lastname"
                 />
             </div>
             <!-- EMAIL -->
@@ -37,6 +49,7 @@
                     id="email"
                     placeholder="email"
                     name="email"
+                    v-model="email"
                 />
             </div>
             <!-- CONTENT -->
@@ -47,14 +60,19 @@
                     id="content"
                     rows="3"
                     name="content"
+                    v-model="content"
                 ></textarea>
             </div>
 
             <!-- BUTTONS -->
             <div class="mb-3">
                 <div class="col-auto">
-                    <button type="submit" class="btn btn-primary mb-3">
-                        Confirm identity
+                    <button
+                        type="submit"
+                        class="btn btn-primary mb-3"
+                        
+                    >
+                        Confirm message
                     </button>
                 </div>
             </div>
@@ -77,6 +95,12 @@ export default {
     data() {
         return {
             doctor: [],
+            // v-model
+            errors: [],
+            firstname: null,
+            lastname: null,
+            email: null,
+            content: null,
         };
     },
     created() {
@@ -89,6 +113,30 @@ export default {
                 console.log(res.data.doctors);
                 this.doctor = res.data.doctors;
             });
+        },
+        checkForm(e) {
+            if (this.firstname && this.lastname && this.email && this.content) {
+                return true;
+            }
+
+
+            this.errors = [];
+
+            if (!this.firstname) {
+                this.errors.push("Firstname required.");
+            }
+            if (!this.lastname) {
+                this.errors.push("lastname required.");
+            }
+            if (!this.email) {
+                this.errors.push("email required.");
+            }
+            if (!this.content) {
+                this.errors.push("content required.");
+            }
+
+console.log(this.errors);
+            e.preventDefault();
         },
     },
 };

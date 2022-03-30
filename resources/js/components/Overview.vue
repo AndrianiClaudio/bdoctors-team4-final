3954ee81a987eb7dcfa0b465d042cb3ac03f56e5
MemeWasTaken <memelazzari02@gmail.com>
2022-03-30 12:48:04 +0200

    Overview

--- resources/js/components/Overview.vue
+++ resources/js/components/Overview.vue
@@ -1,6 +1,48 @@
 <template>
     <div class="container-fluid">
-        
+        <div class="row">
+            <div class="col text-center mt-5">
+                <h1> Come lavoriamo? </h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col d-flex justify-content-center text-center mt-5">
+                <p class="w-50">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero est provident amet deleniti explicabo, accusantium sint odit nulla quas, numquam impedit quam ipsam doloremque fuga unde reiciendis molestias placeat nam.</p>
+            </div>
+        </div>
+
+        <div class="row mt-5 pb-5">
+            <div class="col d-flex flex-column justify-content-center align-items-center">
+                <div class="mask-decoration d-flex flex-column justify-content-center align-items-center rounded-circle mb-5">
+                    <div class="mask-img rounded-circle"> </div>
+                </div>
+                <h2 class="text-center mb-2">Appointment</h2>
+                <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic exercitationem, rem tempora assumenda.</p>
+            </div>
+            <div class="col d-flex flex-column justify-content-center align-items-center">
+                <div class="mask-decoration d-flex flex-column justify-content-center align-items-center rounded-circle mb-5">
+                    <div class="mask-img rounded-circle"> </div>
+                </div>
+                <h2 class="text-center mb-2">Appointment</h2>
+                <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic exercitationem, rem tempora assumenda.</p>
+            </div>
+            <div class="col d-flex flex-column justify-content-center align-items-center">
+                <div class="mask-decoration d-flex flex-column justify-content-center align-items-center rounded-circle mb-5">
+                    <div class="mask-img rounded-circle"> </div>
+                </div>
+                <h2 class="text-center mb-2">Appointment</h2>
+                <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic exercitationem, rem tempora assumenda.</p>
+            </div>
+            <div class="col d-flex flex-column justify-content-center align-items-center">
+                <div class="mask-decoration d-flex flex-column justify-content-center align-items-center rounded-circle mb-5">
+                    <div class="mask-img rounded-circle"> </div>
+                </div>
+                <h2 class="text-center mb-2">Appointment</h2>
+                <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic exercitationem, rem tempora assumenda.</p>
+            </div>
+            
+        </div>
     </div>
 </template>
 
@@ -12,6 +54,26 @@
 
 <style lang="scss" scoped>
     .container-fluid {
-        height: 650px;
+        // height: 650px;
+        background-color: #37417e;
+
+        .row {
+            .col {
+                .mask-decoration {
+                    border:1px dashed rgba(255, 255, 255, 0.6);
+                    width: 250px;
+                    height: 250px;
+
+                    .mask-img {
+                        background-image: url('../../../storage/app/public/images/doctor1.jpg');
+                        background-repeat: no-repeat;
+                        background-size: cover;
+                        background-position: center;
+                        width: 200px;
+                        height: 200px;
+                    }
+                }
+            }
+        }
     }
 </style>

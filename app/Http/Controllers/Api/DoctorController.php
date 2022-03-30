31d3f7b0112424ab17df00ad365d175cc9090944
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-03-30 14:24:47 +0200

    new guest message fix

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
 use App\Model\Message;
-use Illuminate\Support\Facades\Validator;
 
 class DoctorController extends Controller
 {
@@ -31,14 +30,12 @@ class DoctorController extends Controller
     {
         $data = $request->all();
 
-
-        $validator = Validator::make($request->all(), [
+        $request->validate([
             'firstname' => 'required | max:60',
             'lastname' => 'required | max:60',
             'email' => 'required | max:255  | email',
-            'content' => 'required'
+            'content' => 'required',
         ]);
-        dd($validator->fails());
 
         $msg = new Message();
         $msg->fill($data);

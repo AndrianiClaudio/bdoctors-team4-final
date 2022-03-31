74d816849d096c38e2b9d78a5159bc20aa01bafa
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-03-31 12:53:47 +0200

    layout dashboard

--- resources/js/view/App.vue
+++ resources/js/view/App.vue
@@ -1,21 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="container">
-    App
-    <router-view></router-view>
-    <EventCalendar/>
-  </div>
-</template>
-
-<script>
-// import  EventCalendar from '../components/EventCalendar.vue'
-export default {
-  // components: {
-  //   EventCalendar
-  //   },
-  name: 'App',
-}
-=======
     <div class="container-fluid p-0">
 
         <router-view></router-view>
@@ -26,7 +9,6 @@ export default {
     export default {
         name: 'App',
     }
->>>>>>> origin/develop
 </script>
 
 <style lang="scss" scoped>

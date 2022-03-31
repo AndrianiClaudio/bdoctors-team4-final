def3f95f49c4d8bb1c37dc71250c341303672faa
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-03-31 15:51:10 +0200

    add welcome board

--- resources/views/doctors/index.blade.php
+++ resources/views/doctors/index.blade.php
@@ -16,5 +16,16 @@
     --}}
 
 @section('content')
-
+        <div class="container mt-3 text-white">
+            <div class="row">
+                <div class="col">
+                    <div class="welcome-sizes welcome-bg d-flex flex-column justify-content-end ps-2">
+                        <div class="data-sizes position-absolute data-abs ms-2 ps-1 pe-1">qui ci mettiamo la data</div>
+                        <h1 class="text-capitalize">Buongiorno Dr. {{ $doctor->firstname }} {{ $doctor->lastname }}</h1>
+                        <p>Si ricordi sempre di respirare</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
 @endsection

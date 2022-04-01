9410365a1a0078a9e20d9ca75f7889daed7887bb
furvietto <fulvio-94@hotmail.it>
2022-04-01 15:37:23 +0200

    add new slide

--- resources/js/components/Doctor-ov.vue
+++ resources/js/components/Doctor-ov.vue
@@ -2,112 +2,30 @@
     <div class="container-fluid p-0">
         <div class="row m-0">
             <div class="col p-0">
-                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                    <!-- righe sotto -->
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-
-                    <!-- immagini -->
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <div class="prova d-flex justify-content-center pt-5 pb-5">
-                                <div class="card me-5" style="width: 25rem;">
-                                    <img src="/images/doctor1.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                                <div class="card me-5" style="width: 25rem;">
-                                    <img src="/images/doctor1.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                                <div class="card" style="width: 25rem;">
-                                    <img src="/images/doctor1.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
+                <div class="all-product d-flex">
+                    <div class="all-product-container w-90 d-flex">
+                    <div
+                        v-for="(counter, index) in counters"
+                        :key="index"
+                        class="all-product-box d-flex">
+                        <img
+                            :src="productArray[counter.counter].photo"
+                            alt=""
+                            />
+                            <div class="filter">
+                            <h5>select options / quick view</h5>
                             </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="prova d-flex justify-content-center pt-5 pb-5">
-                                <div class="card me-5" style="width: 23rem;">
-                                    <img src="/images/doctor1.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                                <div class="card me-5" style="width: 23rem;">
-                                    <img src="/images/doctor1.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                                <div class="card" style="width: 23rem;">
-                                    <img src="/images/doctor1.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
+                            <h3>{{ productArray[counter.counter].firstname }}</h3>
+                            <p>{{ productArray[counter.counter].lastname }}</p>
+                            <div @click="prev()" class="left-arrow">
+                            <i class="fas fa-chevron-left"></i>
                             </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="prova d-flex justify-content-center pt-5 pb-5">
-                                <div class="card me-5" style="width: 23rem;">
-                                    <img src="/images/doctor1.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                                <div class="card me-5" style="width: 23rem;">
-                                    <img src="/images/doctor1.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                                <div class="card" style="width: 23rem;">
-                                    <img src="/images/doctor1.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
+                            <div @click="next()" class="right-arrow">
+                            <i class="fas fa-chevron-right"></i>
                             </div>
-                        </div>
                     </div>
-
-                    <!-- slider -->
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                     </div>
+                </div>
             </div>
         </div>
     </div>
@@ -116,12 +34,55 @@
 <script>
     export default {
         name: "Doctor-ov",
+        data() {
+            return {
+                productArray:[],
+                 counters: [
+                    {
+                    counter: 0,
+                    },
+                    {
+                    counter: 1,
+                    },
+                    {
+                    counter: 2,
+                    },
+                    {
+                    counter: 3,
+                    },
+                ],
+            }
+        },
+        methods: {
+            prev(index) {
+                this.counters.forEach((element, countIndex) => {
+                    countIndex == index;
+                    element.counter == countIndex;
+                    element.counter--;
+
+                    if (element.counter < 0) {
+                    element.counter = this.productArray.length - 1;
+                    }
+                });
+                },
+                next(index) {
+                this.counters.forEach((element, countIndex) => {
+                    countIndex == index;
+                    element.counter == countIndex;
+                    element.counter++;
+
+                    if (element.counter > this.productArray.length - 1) {
+                    element.counter = 0;
+                    }
+                });
+                },
+        },
        created() {
            axios
                 .get("api/doctors")
                 .then((res) => {
-                    this.doctors = res.data.results.doctors;
-                    console.log(this.doctors);
+                    this.productArray = res.data.results.doctors;
+                    console.log(this.productArray);
                 })
                 .catch((err) => {
                     console.error(err);
@@ -131,12 +92,73 @@
 </script>
 
 <style lang="scss" scoped>
-    .carousel-item{
-        .prova{
-            width: 100%;
-            background-color: red;
-            
-        }
-        
+   .all-product {
+  margin-top: 12em;
+  .heading {
+    h5 {
+      line-height: 2em;
     }
+  }
+  .all-product-container {
+    margin: auto;
+    gap: 1.5em;
+
+    .all-product-box {
+      flex-basis: calc(100% / 4);
+      height: 500px;
+      position: relative;
+      text-align: center;
+      flex-direction: column;
+      justify-content: center;
+      .filter {
+        display: none;
+        width: 100%;
+        height: 82%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 2;
+      }
+      &:nth-of-type(3),
+      &:nth-of-type(4) {
+        .left-arrow,
+        .right-arrow {
+          display: none;
+        }
+      }
+      &:nth-of-type(2) {
+        .right-arrow {
+          display: none;
+        }
+      }
+      &:nth-of-type(5) {
+        .left-arrow {
+          display: none;
+        }
+      }
+      &:hover .filter {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgba(0, 0, 0, 0.5);
+        h5 {
+          cursor: pointer;
+          color: white;
+        }
+      }
+      h3 {
+        margin: 1em;
+        font-size: 1.2em;
+      }
+      p {
+        font-size: 1.2em;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+  }
+}
 </style>

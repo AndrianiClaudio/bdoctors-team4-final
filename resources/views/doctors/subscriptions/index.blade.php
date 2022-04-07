7c8648e3a392128633f930bc1809f1bbca84d0c6
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-04-07 18:29:22 +0200

    stampa data scadenza abbonamento in testo

--- resources/views/doctors/subscriptions/index.blade.php
+++ resources/views/doctors/subscriptions/index.blade.php
@@ -1,39 +1,33 @@
 @extends('layouts.admin')
 @section('script')
     <script src="{{ asset('js/front.js') }}" defer></script>
+    <script src="{{ asset('js/translate_day.js') }}" defer></script>
 @endsection
 
+@php
+$expires = Carbon\Carbon::parse($expires);
+$expires_day_txt = $expires->englishDayOfWeek;
+$expires_week_month = $expires->weekOfMonth;
+// $expires_date = ;
+@endphp
 @section('content')
     <h2>Dr. {{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</h2>
     <div id="fulvio" class="d-none">
         {{ Auth::user()->id }}
     </div>
     <div id="expires">
-       {{--  {{ $expires }} --}}
+        <strong>
+            <em>
+                Hai gi&aacute; un abbonamento attivo che scadr&aacute; <span
+                    id="translate_day">{{ $expires_day_txt }}</span>
+                <span>{{ $expires->day }}</span>
+                <span id="translate_week_month">
+                    {{ $expires->month }}
+                </span>
+                <span>{{ $expires->year }}</span>
+                alle ore
+                <span>{{ $expires->format('H:i') }}</span>
+            </em>
+        </strong>
     </div>
-
-    {{-- @dd(App\Model\Subscription::all()); --}}
-    {{-- @foreach (App\Model\Subscription::all() as $sub)
-        <ul class="list-group">
-            <li id="subscription-{{ $sub->id }}" class="list-group-item list-group-item-info"
-                name="subscription-{{ $sub->id }}">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">{{ $sub->name }}</h4>
-                        <div class="d-flex justify-content-between">
-                            <b>
-                                <em>Price: </em>{{ $sub->price }} &euro;
-                            </b>
-                            <b>
-                                <em>Durata abbonamento: </em>{{ $sub->duration }} ore;
-                            </b>
-                        </div>
-                        <div id="submit" class="submit">
-                            <a href="/checkout?sub ={{ $sub->name }}">Procedi al pagamento</a>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    @endforeach --}}
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,10 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/admin.js', 'public/js')
-    .js('resources/js/front.js', 'public/js')
-    .js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps()
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js("resources/js/admin.js", "public/js")
+    .js("resources/js/front.js", "public/js")
+    .js("resources/js/translate_day.js", "public/js")
+    .js("node_modules/popper.js/dist/popper.js", "public/js")
+    .sourceMaps()
+    .sass("resources/sass/app.scss", "public/css")
     // .sass('resources/sass/dashboard.scss', 'public/css')
-    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
-    
+    .copyDirectory(
+        "node_modules/@fortawesome/fontawesome-free/webfonts",
+        "public/webfonts"
+    );

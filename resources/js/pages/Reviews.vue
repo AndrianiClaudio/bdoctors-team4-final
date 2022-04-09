6bdc09a54cc8ba22ee1c276efc620a48e4d69442
MemeWasTaken <memelazzari02@gmail.com>
2022-04-09 11:14:29 +0200

    Reviews.vue

--- resources/js/pages/Reviews.vue
+++ resources/js/pages/Reviews.vue
@@ -1,6 +1,54 @@
 <template>
-    <div class="container-fluid justify-content-center text-center">
-        <!-- {{ doctor.reviews }} -->
+<div class="container-fluid d-flex justify-content-center align-items-center m-0 p-0">
+    <div class="list-container p-2">
+        <ul v-if="doctor" class="list-group">
+            <li 
+                class="list-group-item"
+                v-for="review in doctor.reviews" :key="review.id"
+            >
+                <div class="row m-0 p-0">
+                    <div class="col d-flex justify-content-start align-items-center m-0 p-0">
+                        <h5 class="d-flex align-items-center mb-0">
+                            Recensione scritta da:
+                                <strong>
+                                    <span v-if="!review.username"> Utente anonimo </span>
+                                    <span v-else> {{review.username}} </span>
+                                </strong>
+                        </h5>
+                        <h6 v-if="review.created_at" class="text-secondary d-flex align-items-start mb-0 ms-2">
+                            In data: {{ review.created_at }}
+                        </h6>
+                    </div>
+                </div>
+                <div class="row m-0p-0">
+                    <div class="col-3 d-flex align-items-center m-0 p-0">
+                        <div class="ms-3 stars">
+                            <span
+                                v-for="(star, index) in review.vote"
+                                :key="`review-${review.id}-star-${index}`"
+                                class="me-2"
+                            >
+                                <i class="bi bi-star-fill"></i></span><span
+                                v-for="(star, index) in 5 - review.vote"
+                                :key="`star-${index}`"
+                                class="me-2"
+                            >
+                                <i class="bi bi-star"></i>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col d-flex align-items-center">
+                        <div class="ms-3 content">
+                            {{ review.content }}
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+    <!-- <div class="container-fluid justify-content-center text-center">
+        {{ doctor.reviews }}
         <ul v-if="doctor">
             <li v-for="review in doctor.reviews" :key="review.id">
                 Recensione scritta da :
@@ -24,7 +72,7 @@
                 <hr />
             </li>
         </ul>
-    </div>
+    </div> -->
 </template>
 
 <script>
@@ -50,4 +98,23 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+    .container-fluid {
+        background-color: #f6f5f8;
+        height: calc(100vh - 70px);
+        .list-container {
+            background-color: #ffffff;
+            border-radius: 19px;
+            width: 60%;
+            height: 650px;
+            overflow: auto;
+        }
+    }
+    .stars {
+        color: #ffcf2f;
+        font-size: 1.7em;
+    }
+    .content {
+        font-size: 1.1em;
+    }
+</style>

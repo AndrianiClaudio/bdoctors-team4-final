57f840c15e2eb062c03c27c132d2f9fa0edd4938
MemeWasTaken <memelazzari02@gmail.com>
2022-04-09 18:30:49 +0200

    Fixato il fixabile

--- resources/js/components/Overview.vue
+++ resources/js/components/Overview.vue
@@ -9,10 +9,8 @@
         <div class="row">
             <div class="col d-flex justify-content-center text-center mt-5">
                 <p class="w-50 text-white">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Libero est provident amet deleniti explicabo, accusantium
-                    sint odit nulla quas, numquam impedit quam ipsam doloremque
-                    fuga unde reiciendis molestias placeat nam.
+                    La nostra piattaforma si basa su tre semplici step per permetterti di ottenere una consulenza o una visita a tempo di record. Scegli tra pi&ugrave; di 10'000 dottori sparsi in tutta Italia: comodamente da casa tua e senza sottoporti a tempi di attesa epopeici.
+
                 </p>
             </div>
         </div>
@@ -24,12 +22,11 @@
                 <div
                     class="mask-decoration d-flex flex-column justify-content-center align-items-center rounded-circle mb-5"
                 >
-                    <div class="mask-img rounded-circle"></div>
+                    <div class="mask-img a rounded-circle"></div>
                 </div>
                 <h2 class="text-center mb-2 text-white">Scegli il medico</h2>
                 <p class="text-center text-white">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Hic exercitationem, rem tempora assumenda.
+                    Cerca tra tutti i medici iscritti alla piattaforma selezionando in base alla specializzazione e alle recensioni degli altri utenti.
                 </p>
             </div>
             <div
@@ -38,14 +35,13 @@
                 <div
                     class="mask-decoration d-flex flex-column justify-content-center align-items-center rounded-circle mb-5"
                 >
-                    <div class="mask-img rounded-circle"></div>
+                    <div class="mask-img b rounded-circle"></div>
                 </div>
                 <h2 class="text-center mb-2 text-white">
                     Richiedi un appuntamento
                 </h2>
                 <p class="text-center text-white">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Hic exercitationem, rem tempora assumenda.
+                    Una volta che sei soddisfatto del medico trovato mandagli un messaggio chiedendogli la disponibilit&agrave; per incontrarsi.
                 </p>
             </div>
             <div
@@ -54,14 +50,13 @@
                 <div
                     class="mask-decoration d-flex flex-column justify-content-center align-items-center rounded-circle mb-5"
                 >
-                    <div class="mask-img rounded-circle"></div>
+                    <div class="mask-img c rounded-circle"></div>
                 </div>
                 <h2 class="text-center mb-2 text-white">
                     Vai all'appuntamento
                 </h2>
                 <p class="text-center text-white">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Hic exercitationem, rem tempora assumenda.
+                    A questo punto non resta che attendere la email di conferma da parte del medico e presentarti all'appuntamento.
                 </p>
             </div>
             <!-- <div class="col d-flex flex-column justify-content-center align-items-center">
@@ -94,12 +89,21 @@ export default {
                 height: 250px;
 
                 .mask-img {
-                    background-image: url("../../../storage/app/public/images/doctor1.jpg");
+                    // background-image: url("../../../storage/app/public/images/doctor1.jpg");
                     background-repeat: no-repeat;
                     background-size: cover;
                     background-position: center;
                     width: 200px;
                     height: 200px;
+                }
+                .mask-img.a {
+                    background-image: url("../../../storage/app/public/images/medici-di-base-716668.large.webp");
+                }
+                .mask-img.b {
+                    background-image: url("../../../storage/app/public/images/uomo-che-messaggia.jpg");
+                }
+                .mask-img.c {
+                    background-image: url("../../../storage/app/public/images/pexels-photo-1919236-2.jpeg");
                 }
             }
         }
